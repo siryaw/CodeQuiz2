dc6b1f6a85b1efc5198186855422fa4fdb6df8a7
siryaw <samappjnr@aol.com>
2021-07-27 20:00:48 -0400

    added completed js. might change questions later

--- questions.js
+++ questions.js
@@ -2,13 +2,13 @@
 let questions = [
     {
     numb: 1,
-    question: "What does HTML stand for?",
-    answer: "Hyper Text Markup Language",
+    question: "How long have I been coding?",
+    answer: "2 years +",
     options: [
-      "Hyper Text Preprocessor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language"
+      "None of my business",
+      "2 years +",
+      "1 year",
+      "6 months"
     ]
   },
     {
@@ -54,4 +54,19 @@ let questions = [
       "eXTra Multi-Program Language",
       "eXamine Multiple Language"
     ]
-  };
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
